7a2af2fa75187ca7bc684e88aadd76b416633ce6
alvnc1 <119267136+alvnc1@users.noreply.github.com>
2023-04-27 20:31:13 -0400

    Delete lib directory

--- lib/Utils/global_colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:hexcolor/hexcolor.dart';
-
-class GlobalColors {
-  static HexColor mainColor = HexColor('#F2BDEF');
-  static HexColor textColor = HexColor('#4F4F4F');
-  static HexColor textColor1 = HexColor('#973DF2');
-}
